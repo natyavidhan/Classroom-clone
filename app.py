6c98a04666b546224c0f992ddd1ab513562764dc
studiousgamer <natyavidhanbiswas10@gmail.com>
2021-10-13 22:57:03 +0530

    added class page and db teacher functions

--- app.py
+++ app.py
@@ -3,13 +3,20 @@ from authlib.integrations.flask_client import OAuth
 import config
 import databases
 from loginpass import create_flask_blueprint, GitHub, Google, Gitlab, Discord
+import pyrebase
 
 app = Flask(__name__)
-oauth = OAuth(app)
-database = databases.Database(config.MONGO_URI)
 
 app.config.from_pyfile('config.py')
 backends = [GitHub, Google, Gitlab, Discord]
+firebaseConfig = config.FIREBASE_CONFIG
+
+oauth = OAuth(app)
+firebase = pyrebase.initialize_app(firebaseConfig)
+fireDB = firebase.database()
+fireStrg = firebase.storage()
+database = databases.Database(config.MONGO_URI, fireStrg, fireDB)
+
 
 @app.route('/')
 def home():
@@ -20,27 +27,86 @@ def home():
 @app.route('/home')
 def dashboard():
     if 'user' in session:
-        return jsonify(session['user'])
-        # if session['user']['type'] == 'teacher':
-        #     return render_template('teacherHome.html', user=session['user'])
-        # return render_template('studentHome.html', user=session['user'])
+        return render_template('home.html', user=session['user'])
     return redirect(url_for('home'))
 
-@app.route('/teacher')
+@app.route('/login')
 def teacher():
-    return render_template('teacher.html')
+    return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('home'))
+
+@app.route('/changeName', methods=['POST'])
+def changeName():
+    if 'user' in session:
+        name = request.form.get('name')
+        if name == '':
+            return 'please enter a valid username'
+        elif len(name) > 20:
+            return 'username too long'
+        user = session['user']
+        user['username'] = name
+        session['user'] = user
+        database.updateName(user['email'], name)
+        return "Updated!"
+    return abort(404)
+
+@app.route('/classes')
+def classes():
+    if 'user' in session:
+        classes = database.getUserClasses(session['user']['email'])
+        classes = [database.getClass(c) for c in classes]
+        return render_template('classes.html', classes=classes, user=session['user'])
+
+@app.route('/classes/new', methods=['GET', 'POST'])
+def newclass():
+    if 'user' in session:
+        if request.method == 'POST':
+            name = request.form.get('name')
+            description = request.form.get('description')
+            database.createClass(session['user']['_id'], name, description)
+            return redirect(url_for('classes'))
+        classes = database.getUserClasses(session['user']['email'])
+        classes = [database.getClass(c) for c in classes]
+        return render_template('addclass.html', classes=classes, user=session['user'])
+
+@app.route('/class/<class_id>')
+def classPage(class_id):
+        if 'user' in session:
+            if database.classExists(class_id):
+                class_ = database.getClass(class_id)
+                if session['user']['_id'] == class_['by']:
+                    return render_template('teacherclass.html', class_=class_, user=session['user'])
+                elif session['user']['_id'] in class_['members']:
+                    return render_template('class.html', class_id=class_id, user=session['user'])
+                else:
+                    return redirect(url_for('home'))
+            else:
+                return "class doesn't exist"
+        return redirect(url_for('home'))
     
+@app.route('/class/<class_id>/assignments')
+def assignments(class_id):
+    if 'user' in session:
+        if database.classExists(class_id):
+            class_ = database.getClass(class_id)
+            assignments = database.getAssignments(class_id)
+            return render_template('assignments.html', class_=class_, assignments=assignments, user=session['user'])
+        else:
+            return "class doesn't exist"
+    else:
+        return redirect(url_for('home'))
+
+
 def handle_authorize(remote, token, user_info):
     if database.userExists(user_info['email']):
-        user = database.getUser(user_info['email'])
-        user['type'] = 'teacher'
-        session['user'] = user
+        session['user'] = database.getUser(user_info['email'])
     else:
         database.addUser(user_info['email'])
-        user = database.getUser(user_info['email'])
-        user['type'] = 'teacher'
-        session['user'] = user
-    # return jsonify(user_info)
+        session['user'] = database.getUser(user_info['email'])
     return redirect(url_for('home'))
 
 
--- databases.py
+++ databases.py
@@ -3,13 +3,28 @@ from uuid import uuid4
 import random
 import datetime
 import requests
+import pyrebase
 
 class Database:
-    def __init__(self, URL):
+    def __init__(self, URL, storage, db):
         self.client = MongoClient(URL)
         self.db = self.client['Classroom-Clone']
         self.users = self.db.users
-        self.data = self.db.data
+        self.classes = self.db.classes
+        self.resources = self.db.resources
+        self.assignments = self.db.assignments
+
+        self.FireDB = db
+        self.FireStrg = storage
+        
+        self.keys = ["A", "B", "C", "D", "E", "F", "G", "H", 
+                     "I", "J", "K", "L", "M", "N", "O", "P", 
+                     "Q", "R", "S", "T", "U", "V", "W", "X", 
+                     "Y", "Z", "0", "1", "2", "3", "4", "5", 
+                     "6", "7", "8", "9", "a", "b", "c", "d", 
+                     "e", "f", "g", "h", "i", "j", "k", "l", 
+                     "m", "n", "o", "p", "q", "r", "s", "t", 
+                     "u", "v", "w", "x", "y", "z"]
         
     def addUser(self, email):
         name = requests.get('http://names.drycodes.com/1').json()[0]
@@ -18,7 +33,9 @@ class Database:
             'username': name,
             'email': email,
             'avatar': f'https://avatars.dicebear.com/api/bottts/{random.randint(100000000000000, 999999999999999999)}.svg',
-            'created': datetime.datetime.now().strftime("%d %B %Y, %I:%M:%S %p")
+            'created': datetime.datetime.now().strftime("%d %B %Y, %I:%M:%S %p"),
+            'classesCreated': [],
+            'classesJoined': []
         })
     
     def userExists(self, email):
@@ -30,3 +47,65 @@ class Database:
     def updateName(self, email, name):
         self.users.update_one({'email': email}, {'$set': {'username': name}})
         return True
+    
+    def createClass(self, userID, name, description):
+        ID = str(uuid4())
+        self.classes.insert_one({
+            '_id': ID,
+            'by': userID,
+            'name': name,
+            'description': description,
+            'image': f"https://avatars.dicebear.com/api/jdenticon/{random.randint(100000000000000, 999999999999999999)}.svg",
+            'created': datetime.datetime.now().strftime("%d %B %Y, %I:%M:%S %p"),
+            'code': ''.join(random.choice(self.keys) for i in range(8)),
+            'members': [userID]
+        })
+        self.users.update_one({'_id': userID}, {'$push': {'classesCreated': ID}})
+        self.users.update_one({'_id': userID}, {'$push': {'classesJoined': ID}})
+        
+    def getClass(self, ID):
+        return self.classes.find_one({'_id': ID})
+    
+    def addMember(self, classID, userID):
+        self.classes.update_one({'_id': classID}, {'$push': {'members': userID}})
+        self.users.update_one({'_id': userID}, {'$push': {'classesJoined': classID}})
+    
+    def removeMember(self, classID, userID):
+        self.classes.update_one({'_id': classID}, {'$pull': {'members': userID}})
+        self.users.update_one({'_id': userID}, {'$pull': {'classesJoined': classID}})
+        
+    def getUserClasses(self, userID):
+        user = self.getUser(userID)
+        print(user)
+        return user['classesJoined']
+        
+    def addResource(self, classID, name, description, link):
+        self.resources.insert_one({
+            '_id': str(uuid4()),
+            'class': classID,
+            'name': name,
+            'description': description,
+            'link': link,
+            'created': datetime.datetime.now().strftime("%d %B %Y, %I:%M:%S %p")
+        })
+        
+    def getResources(self, classID):
+        resources = self.resources.find({'class': classID})
+        return [resource for resource in resources]
+    
+    def addAssignment(self, classID, name, description):
+        self.assignments.insert_one({
+            '_id': str(uuid4()),
+            'class': classID,
+            'name': name,
+            'description': description,
+            'created': datetime.datetime.now().strftime("%d %B %Y, %I:%M:%S %p")
+        })      
+    
+    def getAssignments(self, classID):
+        assignments = self.assignments.find({'class': classID})
+        return [assignment for assignment in assignments]
+        
+    def classExists(self, ID):
+        return self.classes.find_one({'_id': ID}) is not None
+    
